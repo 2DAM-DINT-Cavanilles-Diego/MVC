09db95b266096c69bbe5ed447fe729ea996478f2
dieberalv <dieberalv@PRN5-10369379>
2025-12-17 19:03:17 +0100

    Primera versión del proyecto MVC

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author dieberalv
+ */
+public class Controlador implements ActionListener{
+    
+    //Declaramos las variables para cada clase creada
+    private final Vista view;
+    private final Modelo model;
+    
+    //Constructor
+    public Controlador()
+    
+}
---
+++ src/modelo/Modelo.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author dieberalv
+ */
+public class Modelo {
+    
+    private int numeroUno;
+    private int numeroDos;
+    private int resultado;
+
+    public int getNumeroUno() {
+        return numeroUno;
+    }
+
+    public void setNumeroUno(int numeroUno) {
+        this.numeroUno = numeroUno;
+    }
+
+    public int getNumeroDos() {
+        return numeroDos;
+    }
+
+    public void setNumeroDos(int numeroDos) {
+        this.numeroDos = numeroDos;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+    
+    
+    //Método para hacer la operación
+    public int multiplicar(){
+        resultado = numeroUno * numeroDos;
+        return resultado;
+    }
+           
+    
+}
