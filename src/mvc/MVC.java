bf00470492dbe152321a90dc600ff38d3b017497
dieberalv <dieberalv@PRN5-10369379>
2025-12-17 19:41:45 +0100

    Versión final del proyecto MVC

--- src/mvc/MVC.java
+++ src/mvc/MVC.java
@@ -4,6 +4,10 @@
  */
 package mvc;
 
+import controlador.Controlador;
+import modelo.Modelo;
+import vista.Vista;
+
 /**
  *
  * @author dieberalv
@@ -14,7 +18,21 @@ public class MVC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        //Vamos a llamar todo para poder ejecutar correctamente
+        //Empezamos a hacer instancia de objetos
+        
+        Modelo mod = new Modelo();
+        Vista view = new Vista();
+        
+        //Vamos a llamar al controlador y le enviamos los parámetros que acabamos de crear
+        Controlador ctrl = new Controlador(view,mod);
+        
+        //Llamar al método del controlador iniciar()
+        ctrl.iniciar();
+        
+        //Establecemos ahora que el formulario sea visible
+        view.setVisible(true);
     }
     
 }
